7138bcf2b4de8617f684c8e511ce9854e002c55a
nazeeh-alsaifi <nazeeh.saifi@gmail.com>
2023-12-20 11:54:07 +0400

    creating the form with minimal validation both in front-end and back-end

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+
+    function search(Request $request): array
+    {
+        $validated = $request->validate([
+            'mysqlHost' => 'required',
+            'mysqlDatabase' => 'required',
+            'mysqlUsername' => 'required',
+            'mysqlPassword' => 'required',
+            'mysqlQuery' => 'required',
+        ]);
+
+        dd($request->all());
+        return [];
+    }
 }
